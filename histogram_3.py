2c4148a7a5fad9fc439866cd6c3ce6814fce2fc6
Ahmed-afk <ahmed98wael.m@gmail.com>
2022-04-21 16:13:34 +0200

    histogram

--- histogram_3.py
+++ histogram_3.py
@@ -7,6 +7,7 @@ from matplotlib import pyplot as plt
                     #### Histo of image ####
 
 image = cv.imread("Images_and_Videos/FanArtCompetition.jpg")
+cv.imshow("original", image)
 gray = cv.cvtColor(image,cv.COLOR_BGR2GRAY)
 w,h = gray.shape
 Histogram = np.zeros(256)
@@ -15,8 +16,9 @@ for i in range (w*h):
 	Histogram[gray_1D[0,i]] += 1
 Histogram 
 
-#plt.plot(Histogram)
+           #### Plotting
 plt.plot(Histogram)
+plt.show()
 
 
            #### Equalization ####
@@ -37,21 +39,24 @@ plt.plot(Equalized_Histogram)
 
            #### Showing image after equalization ####
 
-final = np.zeros_like(gray)
-for i in range (w):
-	for j in range (h):
-		final[i,j] = New_levels[gray[i,j]]
+# final = np.zeros_like(gray)                     
+# for i in range (w):
+# 	for j in range (h):
+# 		final[i,j] = New_levels[gray[i,j]]
 
+# cv.imshow("final",final)
+# cv.waitKey(0)
+
+final = np.zeros_like(gray_1D)             #Can show Equalized image by 2 ways, 1)using 2 nested loops and 2x2 array
+for i in range (w*h):                      #or 2)using 1 loop, 1xsize array then reshape it to image dimensions 
+	final[0,i] = New_levels[gray_1D[0,i]]
+
+final = np.reshape(final,(w,h))
 cv.imshow("final",final)
+
+
+
+
+
 cv.waitKey(0)
-
-
-
-
-
-
-
-
-
-
-#plt.show()
+plt.show()
