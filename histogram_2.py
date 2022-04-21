67ff4875a9eeeac13112514024ae69688e6010ca
Ahmed-afk <ahmed98wael.m@gmail.com>
2022-04-21 07:06:41 +0200

    histogram

--- histogram_2.py
+++ histogram_2.py
@@ -6,8 +6,9 @@ from matplotlib import pyplot as plt
 image = cv.imread("Images_and_Videos/girl.jpg")
 gray = cv.cvtColor(image,cv.COLOR_BGR2GRAY)
 #print(gray[250,450])
-#cv.imshow("gray",gray)
-#img = gray.shape
+cv.imshow("gray",gray)
+img = gray.shape
+print(img)
 pre_histogram = np.zeros(256)
 w,h = gray.shape[:2]
 #print(w*h)
@@ -31,18 +32,28 @@ print(sum_histo[255])
 
 new_levels = np.uint8(255*sum_histo)
 print(new_levels[255])
-new_levels = np.round(new_levels)
-plt.plot(new_levels)
+Histogram = np.round(new_levels)
+plt.plot(Histogram)
+plt.bar(Histogram,Histogram)
 plt.show()
 
 print(new_levels[gray[3,100]])
-
+# applying transfered values for each pixels
 New_image = np.zeros(256)
+# y = np.zeros_like(gray)
+# for i in range (w*h):
+# 	New_image[new_levels[image_1D[0,i]]] += 1  
 
-plt.plot(New_image)
-plt.show()
 
+# y = np.reshape(New_image,(w))
+s1, s2 = gray.shape
+Y = np.zeros_like(gray)
+# applying transfered values for each pixels
+for i in range(0, s1):
+	for j in range(0, s2):
+		Y[i, j] = new_levels[gray[i, j]]
 
+cv.imshow("Y",Y)
 
 
 cv.waitKey(0)
